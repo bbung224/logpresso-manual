b96ede08ca4384e971b1d047f6565797c3110427
xeraph <xeraph@eediom.com>
2014-03-25 23:38:12 +0900

    added cli manual sections 3.1~3.8

--- cli/ko/pdfbuild.bat
+++ cli/ko/pdfbuild.bat
@@ -4,4 +4,12 @@ pandoc ^
 1.3.license.md ^
 1.4.benchmark.md ^
 2.1.client_tools.md ^
+3.1.accounting.md ^
+3.2.query.md ^
+3.3.table.md ^
+3.4.crypto.md ^
+3.5.import.md ^
+3.6.parser.md ^
+3.7.index.md ^
+3.8.cache.md ^
 --toc --toc-depth=2 --template=cli.tex --latex-engine=xelatex -V geometry:"top=3.5cm, bottom=3.5cm, left=2.5cm, right=2.5cm" -s -o logpresso_cli_manual.pdf
