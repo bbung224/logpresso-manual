31a26c5236a40e972b4b00bc8a955f4d0b17731b
everclear-- <faditia@gmail.com>
2014-07-04 17:23:56 +0900

    added 12. troubleshooting

--- cli/ko/pdfbuild.sh
+++ cli/ko/pdfbuild.sh
@@ -47,4 +47,5 @@ pandoc \
 9.1.alert_config.md \
 10.1.excel_external.md \
 11.1.hdfs_connecter.md \
+12.1.troubleshooting.md \
 --toc --toc-depth=2 --template=cli.tex --latex-engine=xelatex -V geometry:"top=3.5cm, bottom=3.5cm, left=2.5cm, right=2.5cm" -s -o logpresso_cli_manual.pdf
